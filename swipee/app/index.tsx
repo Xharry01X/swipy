7a03238b873994894468fad48d8ad195eefbb3dd
harshit-506 <supriyamohapatra658@gmail.com>
2024-08-16 11:16:25 +0530

    refactoring done...

---
+++ swipee/app/index.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+
+
+export default function HomeScreen() {
+  return (
+   <>
+   <View>
+   <Text>Hii Harry</Text>
+   </View>
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
